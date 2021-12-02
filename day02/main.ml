b7b0187c722f7fa7a92e7297539a07efa8fc3202
Bouillaguet Quentin <git@desplandis.fr>
2021-12-02 12:06:39 +0100

    Day 2 with one position type

--- day02/main.ml
+++ day02/main.ml
@@ -1,7 +1,6 @@
 open Types
 
-type pos = { x: int; y: int }
-type state = { pos: pos; aim: int }
+type pos = { x: int; y: int; aim: int }
 
 let pp_instr fmt = function
   | Up i -> Format.fprintf fmt "Up %d" i
@@ -22,15 +21,16 @@ let move pos = function
   | Up i -> { pos with y = pos.y - i }
   | Forward i -> { pos with x = pos.x + i }
 
-let update state = function
-  | Down i -> { state with aim = state.aim + i }
-  | Up i -> { state with aim = state.aim - i }
-  | Forward i ->
-    let pos = { x = state.pos.x + i; y = state.pos.y + (i * state.aim) } in
-    { state with pos}
+let update pos = function
+  | Down i -> { pos with aim = pos.aim + i }
+  | Up i -> { pos with aim = pos.aim - i }
+  | Forward i -> { pos with
+      x = pos.x + i;
+      y = pos.y + (i * pos.aim)
+  }
 
-let execute_instrs ~to_pos f start instrs =
-  List.fold_left f start instrs |> to_pos |>
+let execute_instrs f start instrs =
+  List.fold_left f start instrs |>
   fun pos -> pos.x * pos.y
 
 let () =
@@ -42,11 +42,10 @@ let () =
     instrs; *)
     close_in chan; instrs
   in
-  let pos = { x = 0; y = 0 } in
-  let state = { pos; aim = 0} in
+  let pos = { x = 0; y = 0; aim = 0 } in
   Format.printf "Score 1: %a\n"
     (Format.pp_print_option Format.pp_print_int)
-    (Option.map (execute_instrs move ~to_pos:(fun pos -> pos) pos) instrs);
+    (Option.map (execute_instrs move pos) instrs);
   Format.printf "Score 2: %a\n"
     (Format.pp_print_option Format.pp_print_int)
-    (Option.map (execute_instrs ~to_pos:(fun s -> s.pos) update state) instrs)
+    (Option.map (execute_instrs update pos) instrs)
